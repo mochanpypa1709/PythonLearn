35cf530900b257dd72503d06f9a31da18eaa44e4
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-03-11 06:57:53 +0700

    Update basics.py

--- Basic/basics.py
+++ Basic/basics.py
@@ -5,13 +5,11 @@ mood = 'Good'
 strength = 9.7
 rank = 2
 grade = [9.1, 8.8, 9.0]
-grades = list(range(1,10))
-grades_odd = list(range(1,10,3))
 rainfall = [23.5, 17, 'hard', list(range(1,10))]
 
+print(mood.upper())
+print(mood.title())
 print(day_hours * week_day)
 print(24 * 7)
 print(week_hours,week_day)
 print(grade)
-print(grades)
-print(grades_odd)
