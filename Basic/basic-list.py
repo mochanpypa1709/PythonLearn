fb27906bc3807b27435dca7cc3c09afc36f9f41c
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-03-11 06:57:45 +0700

    Create basic-list.py

---
+++ Basic/basic-list.py
@@ -0,0 +1,10 @@
+grades = list(range(1,10))
+print(grades)
+
+grades_odd = list(range(1,10,3))
+print(grades_odd)
+
+
+student_grades = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+count = list.count(student_grades, 10.0)
+print(count)
