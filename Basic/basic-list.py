0e2b9344bb776a287c5cf8b2fbde473747f74e17
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-03-11 07:25:06 +0700

    Update basic-list.py

--- Basic/basic-list.py
+++ Basic/basic-list.py
@@ -6,5 +6,11 @@ print(grades_odd)
 
 
 student_grades = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+student_grades.append(6.8)
+print(student_grades)
+
+student_grades.remove(9.1)
+print(student_grades)
+
 count = list.count(student_grades, 10.0)
 print(count)
