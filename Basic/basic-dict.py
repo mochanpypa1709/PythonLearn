dedf5213d3d9197099bff1b1e73616a2ffe59f66
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-03-11 07:09:25 +0700

    Create basic-dict.py

---
+++ Basic/basic-dict.py
@@ -0,0 +1,4 @@
+student_grades = {"Leon": 9.1, "Rein": 8.8, "Dzaky": 10.0, "Pypa": 7.7, "Myma": 6.8}
+print(student_grades.values())
+print(student_grades.keys())
+print(sum(student_grades.values()))
