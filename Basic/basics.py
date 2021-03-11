5fb00c9afacdbb0451d598ab75294e7e97b0a767
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-03-11 07:09:43 +0700

    Update basics.py

--- Basic/basics.py
+++ Basic/basics.py
@@ -1,14 +1,11 @@
 day_hours = 24
 week_day = 7
 week_hours = day_hours * week_day
-mood = 'Good'
 strength = 9.7
 rank = 2
 grade = [9.1, 8.8, 9.0]
 rainfall = [23.5, 17, 'hard', list(range(1,10))]
 
-print(mood.upper())
-print(mood.title())
 print(day_hours * week_day)
 print(24 * 7)
 print(week_hours,week_day)
