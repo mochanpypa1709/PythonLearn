c4811b1de08588be3797395ad63dbb94d73eef65
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-05 15:33:13 +0700

    Update cond.py

--- Basic-func/cond.py
+++ Basic-func/cond.py
@@ -12,3 +12,29 @@ if x == 1 or y==2:
     print("Yes")
 else:
     print("No")
+
+#conditional block
+message = "hello there"
+ 
+if "hello" in message:
+    print("hi")
+elif "hi" in message:
+    print("hi")
+elif "hey" in message:
+    print("hi")
+else:
+    print("I don't understand")
+
+#check value
+#message = "hello"
+message = [1,2,3]
+
+if isinstance(message,str):
+    print ('This is string')
+elif isinstance(message,list):
+    print ('This is list')
+
+if type(message) == str:
+    print ('This is string')
+elif type(message) == list:
+    print ('This is list')
