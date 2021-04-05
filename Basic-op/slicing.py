04f657bfd57d21fd7f6253f82f8d59050bc11e21
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-05 14:55:55 +0700

    Create slicing.py

---
+++ Basic-op/slicing.py
@@ -0,0 +1,16 @@
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+
+#print out b,c,d
+print(letters[1:4])
+
+#print out a,b,c,
+print(letters[:3])
+
+#print last three
+print(letters[4:])
+
+
+#["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+#  -7     -6     -5     -4     -3     -2     -1
+print(letters[-3:])
+print(letters[:-1])
