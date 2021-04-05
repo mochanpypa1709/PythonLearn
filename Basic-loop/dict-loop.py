308e2540d949e35cb5a8a10d283682f14420f244
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-05 22:33:41 +0700

    Update dict-loop.py

--- Basic-loop/dict-loop.py
+++ Basic-loop/dict-loop.py
@@ -1,8 +1,19 @@
 phone_numbers = {"John Smith": "+37682929928", "Marry Simpons": "+423998200919"}
  
+for value in phone_numbers.keys():
+    print(value)
+
+for value in phone_numbers.values():
+    print(value)
+
 for pair in phone_numbers.items():
     print("{} has as phone number {}".format(pair[0], pair[1]))
 
-
 for key, value in phone_numbers.items():
     print("{} has as phone number {}".format(key, value))
+
+for key, value in phone_numbers.items():
+    print("%s: %s" % (key, value))
+
+for value in phone_numbers.values():
+    print(value.replace("+", "00"))
