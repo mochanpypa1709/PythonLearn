d42714c9a48979c6631cbaefd40a24d400d044c3
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-06 14:08:35 +0700

    Update user_input.py

--- Basic-process/user_input.py
+++ Basic-process/user_input.py
@@ -7,3 +7,4 @@ print("Hi {}, you have {} years of experience".format(name, experience_years))
 experience_months = input("Enter your experience in months: ")
 experience_years = int(experience_months) / 12
 print(experience_years)
+
