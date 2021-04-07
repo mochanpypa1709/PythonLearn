ed12eb49ce9ec8e48f2432ea3e3d6d68e3c2c4ef
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-07 17:19:41 +0700

    Update mod_1.py

--- modules/mod_1.py
+++ modules/mod_1.py
@@ -1,7 +1,11 @@
 import time
+import os
 
 #exec print after 10sec
 while True:
-    with open("file-process/vegetables.txt") as file:
-        print(file.read())
-        time.sleep(10)
+    if os.path.exists("file-process/vegetables.txt"):
+        with open("file-process/vegetables.txt") as file:
+            print(file.read())
+    else:
+         print("File does not exist")
+    time.sleep(10)
