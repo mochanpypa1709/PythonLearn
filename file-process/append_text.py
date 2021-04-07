0a403f83222bafc621bdc9eac543f5ac468efae7
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-07 17:19:38 +0700

    Update append_text.py

--- file-process/append_text.py
+++ file-process/append_text.py
@@ -5,3 +5,11 @@ with open("file-process/fruits.txt", "a+") as myfile:
     content = myfile.read()
 
 print(content)
+
+
+#read and append
+with open("bear1.txt") as file:
+    content = file.read()
+
+with open("bear2.txt", "a") as file:
+    file.write(content)
