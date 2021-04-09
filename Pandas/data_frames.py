be627f4e3551f72180eb0f7246f75bd69bace026
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-09 22:38:28 +0700

    Update data_frames.py

--- Pandas/data_frames.py
+++ Pandas/data_frames.py
@@ -3,4 +3,5 @@ df1 = pandas.DataFrame([[2,4,6],[10,20,30]])
 df2 = pandas.DataFrame([[2,4,6],[10,20,30]], columns = ["Price","Age","Value"], index = ["First","Second"])
 df3 = pandas.DataFrame([{"Name":"John","Surname":"Ji"},{"Name":"Jack"}])
 print(df3)
-
+print(df2.mean())
+print(df2.Price)
