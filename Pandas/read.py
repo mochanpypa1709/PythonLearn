c492096c87aa62c7d4565e48bd5968b90dc3a100
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-09 23:36:12 +0700

    Update read.py

--- Pandas/read.py
+++ Pandas/read.py
@@ -4,4 +4,9 @@ df2=pandas.read_json("Pandas/supermarkets.json")
 df3=pandas.read_excel("Pandas/supermarkets.xlsx",sheet_name=0)
 df4=pandas.read_csv("Pandas/supermarkets-commas.txt")
 df5=pandas.read_csv("Pandas/supermarkets-semi-colons.txt",sep=';')
-print(df5)
+
+#add header
+df6=pandas.read_csv("Pandas/no_header.txt")
+df6.columns = ("ID","Address","City","Zip","Country","Name","Employee")
+
+print(df6)
