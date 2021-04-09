bcec760fd02fb031eeff4449970f5fdc478526b9
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-09 23:50:14 +0700

    Create index_slice.py

---
+++ Pandas/index_slice.py
@@ -0,0 +1,8 @@
+import pandas
+df2=pandas.read_json("Pandas/supermarkets.json")
+df2=df2.set_index("Address")
+locate = df2.loc["332 Hill St","Country"]
+slicing = df2.iloc[3,1:4]
+print(df2)
+print(locate)
+print(slicing)
