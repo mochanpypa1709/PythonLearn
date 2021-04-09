35402ea03e330abe0b47c2f3a5d844be5bad0b4d
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-09 23:26:16 +0700

    Update read.py

--- Pandas/read.py
+++ Pandas/read.py
@@ -1,4 +1,7 @@
 import pandas
 df1=pandas.read_csv("Pandas/supermarkets.csv")
 df2=pandas.read_json("Pandas/supermarkets.json")
-print(df2)
+df3=pandas.read_excel("Pandas/supermarkets.xlsx",sheet_name=0)
+df4=pandas.read_csv("Pandas/supermarkets-commas.txt")
+df5=pandas.read_csv("Pandas/supermarkets-semi-colons.txt",sep=';')
+print(df5)
