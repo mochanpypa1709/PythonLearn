924018ffe1b5c5a2f677896af3f3c4ddba4dc5bf
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-10 00:09:31 +0700

    Update index_slice.py

--- Pandas/index_slice.py
+++ Pandas/index_slice.py
@@ -4,7 +4,12 @@ df2=df2.set_index("Address")
 locate = df2.loc["332 Hill St","Country"]
 slicing = df2.iloc[3,1:4]
 drop = df2.drop("332 Hill St")
+#add column
+df2["Continent"] = df2.shape[0]*["North America"]
+#update column
+df2["Continent"] = df2["Country"]+ ", " + "North America"
+#print(df2)
+#print(locate)
+#print(slicing)
+#print(drop)
 print(df2)
-print(locate)
-print(slicing)
-print(drop)
