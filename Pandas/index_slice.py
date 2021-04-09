efb362f7bb1fd533ecde423af092dbc2d9b1f5ac
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-09 23:54:46 +0700

    Update index_slice.py

--- Pandas/index_slice.py
+++ Pandas/index_slice.py
@@ -3,6 +3,8 @@ df2=pandas.read_json("Pandas/supermarkets.json")
 df2=df2.set_index("Address")
 locate = df2.loc["332 Hill St","Country"]
 slicing = df2.iloc[3,1:4]
+drop = df2.drop("332 Hill St")
 print(df2)
 print(locate)
 print(slicing)
+print(drop)
