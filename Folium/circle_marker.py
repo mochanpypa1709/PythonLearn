14a00771edabf19f17fad0037570a453a4262f16
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-14 23:00:54 +0700

    Update circle_marker.py

--- Folium/circle_marker.py
+++ Folium/circle_marker.py
@@ -18,15 +18,24 @@ Height: %s m
 """
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles ="Stamen Terrain")
-fg = folium.FeatureGroup(name="My Map")
+
+fgv = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, el in zip(lat, lon, elev):
     iframe = folium.IFrame(html=html % str(el), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=folium.Popup(iframe), fill_color=color_producer(el), color ='grey', fill_opacity=0.7))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=folium.Popup(iframe), fill_color=color_producer(el), color ='grey', fill_opacity=0.7))
 
-fg.add_child(folium.GeoJson(data=open('Folium/world.json', 'r', encoding='utf-8-sig').read(), 
+#geojson polygon layer & choropleth
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('Folium/world.json', 'r', encoding='utf-8-sig').read(), 
 style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red' }))
 
-map.add_child(fg)
+map.add_child(fgv)
+map.add_child(fgp)
+
+#layer control panel
+map.add_child(folium.LayerControl())
+
 map.save("Folium/circle_map.html")
