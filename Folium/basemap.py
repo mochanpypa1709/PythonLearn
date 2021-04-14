31b8cb66c7bd70e1705517c0ae920bc8bb516678
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-14 21:46:38 +0700

    Update basemap.py

--- Folium/basemap.py
+++ Folium/basemap.py
@@ -5,7 +5,9 @@ import folium
 map = folium.Map(location=[-6.80, 108], zoom_start=8, tiles ="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[-6.56, 106.78], popup="Hi, I Am Marker", icon=folium.Icon(color='blue')))
-map.add_child(fg)
 
+for coordinate in [[-6.56, 106.78],[-6.40, 105.78]]:
+    fg.add_child(folium.Marker(location=coordinate, popup="Hi, I Am Marker", icon=folium.Icon(color='blue')))
+
+map.add_child(fg)
 map.save("Folium/Map2.html")
