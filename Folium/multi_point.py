4bc8c51e2ceb48b18c1aa4493ccf93cd9299f819
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-14 22:01:37 +0700

    Update multi_point.py

--- Folium/multi_point.py
+++ Folium/multi_point.py
@@ -3,13 +3,14 @@ import pandas
 data = pandas.read_csv("Folium/Volcanoes.txt")
 lon = list(data["LON"])
 lat = list(data["LAT"])
+elev = list(data["ELEV"])
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles ="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
-    fg.add_child(folium.Marker(location=[lt, ln], popup="Hi, I Am Marker", icon=folium.Icon(color='blue')))
+for lt, ln, el in zip(lat, lon, elev):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color='blue')))
 
 map.add_child(fg)
 map.save("Folium/Map3.html")
