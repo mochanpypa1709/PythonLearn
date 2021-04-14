8214910f9cb1a075d749f51b7768387081772c21
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-14 22:11:39 +0700

    Update html_pop.py

--- Folium/html_pop.py
+++ Folium/html_pop.py
@@ -5,6 +5,14 @@ lon = list(data["LON"])
 lat = list(data["LAT"])
 elev = list(data["ELEV"])
 
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000<= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
 html = """<h4>Volcano information:</h4>
 Height: %s m
 """
@@ -14,7 +22,7 @@ fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat, lon, elev):
     iframe = folium.IFrame(html=html % str(el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='blue')))
+    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_producer(el))))
 
 map.add_child(fg)
 map.save("Folium/popup_iframe.html")
