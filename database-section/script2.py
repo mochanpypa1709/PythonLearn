0495b3af40ee0ad46c1270d4f74b069b802b7bf5
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-25 20:16:10 +0700

    Update script2.py

--- database-section/script2.py
+++ database-section/script2.py
@@ -1,21 +1,21 @@
-import pyscopg2
+import mysql.connector
 
 def create_table():
-    conn = sqlite3.connect("database-section/lite.db")
+    conn = mysql.connector.connect(database="learnpython", user="root", password="", host="localhost")
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
     conn.commit()
     conn.close()
 
 def insert(item, qty, price):
-    conn = sqlite3.connect("database-section/lite.db")
+    conn = mysql.connector.connect(database="learnpython", user="root", password="", host="localhost")
     cur = conn.cursor()
-    cur.execute("INSERT INTO store VALUES (?,?,?)",(item, qty, price))
+    cur.execute("INSERT INTO store VALUES (%s,%s,%s)",(item, qty, price))
     conn.commit()
     conn.close()
 
 def view():
-    conn = sqlite3.connect("database-section/lite.db")
+    conn = mysql.connector.connect("database-section/lite.db")
     cur = conn.cursor()
     cur.execute("SELECT * FROM store")
     rows=cur.fetchall()
@@ -23,21 +23,21 @@ def view():
     return rows
 
 def delete(item):
-    conn = sqlite3.connect("database-section/lite.db")
+    conn = mysql.connector.connect("database-section/lite.db")
     cur = conn.cursor()
     cur.execute("DELETE FROM store WHERE item=?", (item,))
     conn.commit()
     conn.close()
 
 def update(qyt,price,item):
-    conn = sqlite3.connect("database-section/lite.db")
+    conn = mysql.connector.connect("database-section/lite.db")
     cur = conn.cursor()
     cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?", (qyt,price,item))
     conn.commit()
     conn.close()
 
-#create_table()
-#insert("Kopi", 5, 18)
+create_table()
+insert("Coklat", 3, 25)
 #delete("Kopi")
-update(6,20,"Kopi")
-print(view())
+#update(6,20,"Kopi")
+#print(view())
