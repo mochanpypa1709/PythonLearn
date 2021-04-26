f32d7407a5eb4904fa3becfecbc0a53d95adc98c
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-04-26 22:23:48 +0700

    Update book_store.py

--- Book-Store-GUI/book_store.py
+++ Book-Store-GUI/book_store.py
@@ -1,10 +1,42 @@
 from tkinter import *
 import data_store
- 
+
 # Create an empty Tkinter window
 window=Tk()
 
- 
+#function 
+def get_selected_row(event):
+    global selected_tuple
+    index=list1.curselection()[0]
+    selected_tuple = list1.get(index)
+    e1.delete(0,END)
+    e1.insert(END,selected_tuple[1])
+    e2.delete(0,END)
+    e2.insert(END,selected_tuple[2])
+    e3.delete(0,END)
+    e3.insert(END,selected_tuple[3])
+    e4.delete(0,END)
+    e4.insert(END,selected_tuple[4])
+
+def view_command():
+    list1.delete(0,END)
+    for row in data_store.view():
+        list1.insert(END,row)
+
+def search_command():
+    list1.delete(0,END)
+    for row in data_store.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()):
+        list1.insert(END,row)
+
+def add_command():
+    data_store.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+    list1.delete(0,END)
+    list1.insert(END,(title_text.get(),author_text.get(),year_text.get(),isbn_text.get()))
+
+def delete_command():
+    data_store.delete(selected_tuple[0])
+
+
 #create label
 l1=Label(window,text="Title")
 l1.grid(row=0,column=0)
@@ -45,20 +77,22 @@ sb1.grid(row=2,column=2,rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
+list1.bind('<<ListboxSelect>>',get_selected_row)
+
 # Create a button widget
-b1=Button(window,text="View all",width=12)
+b1=Button(window,text="View all",width=12,command=view_command)
 b1.grid(row=2,column=3)
 
-b2=Button(window,text="Search entry",width=12)
+b2=Button(window,text="Search entry",width=12,command=search_command)
 b2.grid(row=3,column=3)
 
-b3=Button(window,text="Add entry",width=12)
+b3=Button(window,text="Add entry",width=12,command=add_command)
 b3.grid(row=4,column=3)
 
 b4=Button(window,text="Update",width=12)
 b4.grid(row=5,column=3)
 
-b5=Button(window,text="Delete",width=12)
+b5=Button(window,text="Delete",width=12,command=delete_command)
 b5.grid(row=6,column=3)
 
 b6=Button(window,text="Close",width=12)
